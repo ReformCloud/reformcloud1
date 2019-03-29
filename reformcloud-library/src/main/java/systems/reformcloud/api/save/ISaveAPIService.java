57fdf9589f9b1823cacbed6401bac829c39704b7
Pasqual K <pasqual.koschmieder@gmail.com>
2019-03-29 17:13:56 +0100

    Added small documentation for the api

---
+++ reformcloud-library/src/main/java/systems/reformcloud/api/save/ISaveAPIService.java
@@ -0,0 +1,12 @@
+/*
+  Copyright © 2019 Pasqual K. | All rights reserved
+ */
+
+package systems.reformcloud.api.save;
+
+/**
+ * @author _Klaro | Pasqual K. / created on 29.03.2019
+ */
+
+public interface ISaveAPIService {
+}
