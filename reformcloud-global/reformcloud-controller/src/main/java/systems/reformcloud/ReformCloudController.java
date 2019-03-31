40394d239ddb00171ddb110011b7d012603e2b84
Pasqual K <pasqual.koschmieder@gmail.com>
2019-03-31 11:48:00 +0200

    Modified README

--- reformcloud-global/reformcloud-controller/src/main/java/systems/reformcloud/ReformCloudController.java
+++ reformcloud-global/reformcloud-controller/src/main/java/systems/reformcloud/ReformCloudController.java
@@ -200,7 +200,7 @@ public class ReformCloudController implements Shutdown, Reload, IAPIService {
         this.shutdownHook = new Thread(this::shutdownAll, "Shutdown-Hook");
         Runtime.getRuntime().addShutdownHook(this.shutdownHook);
 
-        //TODO: Remove 2
+        //TODO: Remove
 
         this.addonParallelLoader.enableAddons();
         this.checkForUpdates();
