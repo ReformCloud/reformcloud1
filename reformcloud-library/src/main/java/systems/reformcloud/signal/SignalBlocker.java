bfffebf51c2f403aac3e3ca71351dcf07bebad11
Pasqual K <pasqual.koschmieder@gmail.com>
2019-04-10 21:57:37 +0200

    Removed SignalBlocker 'y' feature

--- reformcloud-library/src/main/java/systems/reformcloud/signal/SignalBlocker.java
+++ reformcloud-library/src/main/java/systems/reformcloud/signal/SignalBlocker.java
@@ -7,13 +7,8 @@ package systems.reformcloud.signal;
 import sun.misc.Signal;
 import sun.misc.SignalHandler;
 import systems.reformcloud.ReformCloudLibraryServiceProvider;
-import systems.reformcloud.utility.ExitUtil;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.Serializable;
-import java.nio.charset.StandardCharsets;
 
 /**
  * @author _Klaro | Pasqual K. / created on 06.04.2019
@@ -35,17 +30,7 @@ public final class SignalBlocker implements Serializable {
     private static final class Handler implements SignalHandler, Serializable {
         @Override
         public void handle(Signal signal) {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
-            ReformCloudLibraryServiceProvider.getInstance().getLoggerProvider().serve("If you really want to exit the application print 'y'");
             ReformCloudLibraryServiceProvider.getInstance().getLoggerProvider().serve("Please note that only the stop command is supported");
-
-            try {
-                if (bufferedReader.readLine().equalsIgnoreCase("y"))
-                    System.exit(ExitUtil.TERMINATED);
-                else
-                    ReformCloudLibraryServiceProvider.getInstance().getLoggerProvider().info("You cancelled the termination");
-            } catch (final IOException ignored) {
-            }
         }
     }
 }
