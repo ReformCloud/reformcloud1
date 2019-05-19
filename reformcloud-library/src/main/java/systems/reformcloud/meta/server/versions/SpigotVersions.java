1a8248aeaed8af08fb4cfa2d7daa38a8a07d9dd0
KlaroYT <pasqual.koschmieder@gmail.com>
2019-05-19 17:10:58 +0200

    Paper 1.14.1 support

--- reformcloud-library/src/main/java/systems/reformcloud/meta/server/versions/SpigotVersions.java
+++ reformcloud-library/src/main/java/systems/reformcloud/meta/server/versions/SpigotVersions.java
@@ -39,8 +39,7 @@ public enum SpigotVersions implements Serializable {
     SPIGOT_1_13_1("Spigot 1.13.1", "1.13.1", "https://mcmirror.io/files/Spigot/Spigot-1.13.1-f6a273b_1ceee63-20180926-0919.jar"),
     SPIGOT_1_13_2("Spigot 1.13.2", "1.13.2", "https://mcmirror.io/files/Spigot/Spigot-1.13.2-0c02b0c-20190425-0538.jar"),
     SPIGOT_1_14("Spigot 1.14", "1.14", "https://mcmirror.io/files/Spigot/Spigot-1.14-8043ebc-20190514-0000.jar"),
-    //TODO
-    SPIGOT_1_14_1("Spigot 1.14.1", "1.14.1", ""),
+    SPIGOT_1_14_1("Spigot 1.14.1", "1.14.1", "https://cdn.getbukkit.org/spigot/spigot-1.14.1.jar"),
     /**
      * Paper Versions
      */
@@ -55,6 +54,7 @@ public enum SpigotVersions implements Serializable {
     PAPER_1_11_2("Paper 1.11.2", "1.11.2", "https://archive.mcmirror.io/Paper/Paper-1.11.2-b1000.jar"),
     PAPER_1_12_2("Paper 1.12.2", "1.12.2", "https://mcmirror.io/files/Paper/Paper-1.12.2-ac69748-20181207-0309.jar"),
     PAPER_1_13_2("Paper 1.13.2", "1.13.2", "https://mcmirror.io/files/Paper/Paper-1.13.2-fb25dc1-20190422-2136.jar"),
+    PAPER_1_14_1("Paper 1.14.1", "1.14.1", "https://papermc.io/api/v1/paper/1.14.1/28/download"),
     /**
      * SpongeVanilla Versions
      */
