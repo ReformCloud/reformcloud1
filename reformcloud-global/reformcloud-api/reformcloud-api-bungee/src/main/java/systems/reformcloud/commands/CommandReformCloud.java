272c30908cde4324c919f20c17db6f872e95c952
JanDragon <40271530+JanDragon@users.noreply.github.com>
2019-08-04 20:35:53 +0200

    Addee /process stopgroup in Help Message

--- reformcloud-global/reformcloud-api/reformcloud-api-bungee/src/main/java/systems/reformcloud/commands/CommandReformCloud.java
+++ reformcloud-global/reformcloud-api/reformcloud-api-bungee/src/main/java/systems/reformcloud/commands/CommandReformCloud.java
@@ -201,6 +201,7 @@ public final class CommandReformCloud extends Command implements Serializable, T
             } else {
                 commandSender.sendMessage(TextComponent
                     .fromLegacyText(prefix + "/reformcloud process <start/stop> <group/name>"));
+                    .fromLegacyText(prefix + "/reformcloud process <stopgroup> <group>"));
             }
 
             return;
