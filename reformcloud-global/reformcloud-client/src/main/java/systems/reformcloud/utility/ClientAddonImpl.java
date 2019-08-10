0d9c967c0e1036d6513a6087c4502c32b17ac8cc
Klaro <pasqual.koschmieder@gmail.com>
2019-08-10 14:03:09 +0200

    Fixed build bug

--- reformcloud-global/reformcloud-client/src/main/java/systems/reformcloud/utility/ClientAddonImpl.java
+++ reformcloud-global/reformcloud-client/src/main/java/systems/reformcloud/utility/ClientAddonImpl.java
@@ -7,10 +7,15 @@ package systems.reformcloud.utility;
 import systems.reformcloud.ReformCloudClient;
 import systems.reformcloud.ReformCloudLibraryServiceProvider;
 import systems.reformcloud.addons.JavaAddon;
+import systems.reformcloud.addons.dependency.DependencyLoader;
+import systems.reformcloud.addons.dependency.util.DynamicDependency;
 import systems.reformcloud.commands.utility.Command;
 import systems.reformcloud.event.utility.Listener;
+import systems.reformcloud.utility.future.DefaultTask;
+import systems.reformcloud.utility.future.Task;
 
 import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * @author _Klaro | Pasqual K. / created on 27.12.2018
@@ -26,6 +31,16 @@ public class ClientAddonImpl extends JavaAddon<ReformCloudClient> {
     @Override
     public ReformCloudLibraryServiceProvider getInternalCloudLibraryService() {
         return ReformCloudLibraryServiceProvider.getInstance();
+    }
+
+    @Override
+    public Task<Void> loadDependencyAsync(DynamicDependency dynamicDependency) {
+        Task<Void> task = new DefaultTask<>();
+        CompletableFuture.runAsync(() -> {
+            DependencyLoader.loadDependency(dynamicDependency);
+            task.complete(null);
+        });
+        return task;
     }
 
     public void registerCommand(final Command command) {
