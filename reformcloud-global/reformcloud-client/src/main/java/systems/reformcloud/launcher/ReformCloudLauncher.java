3f70637f396344c5b3e1ea6bea1f0ede8d3962f6
robertschuck <43842862+robertschuck@users.noreply.github.com>
2019-08-31 14:12:57 +0200

    Removed "ignore-root" parameter (Client)
    "--ignore-root" parameter removed to protect users.

--- reformcloud-global/reformcloud-client/src/main/java/systems/reformcloud/launcher/ReformCloudLauncher.java
+++ reformcloud-global/reformcloud-client/src/main/java/systems/reformcloud/launcher/ReformCloudLauncher.java
@@ -40,8 +40,7 @@ final class ReformCloudLauncher implements Serializable {
         final List<String> options = Arrays.asList(args);
 
         if (StringUtil.USER_NAME.equals("root")
-            && StringUtil.OS_NAME.toLowerCase().contains("linux")
-            && !options.contains("--ignore-root")) {
+            && StringUtil.OS_NAME.toLowerCase().contains("linux")) {
             System.out.println("You cannot run ReformCloud as root user");
             try {
                 Thread.sleep(2000);
